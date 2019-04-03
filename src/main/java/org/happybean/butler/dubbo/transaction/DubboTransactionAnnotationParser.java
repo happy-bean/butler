ea386b8111a3c95d2611d4d480fe8328c05838bc
wgt <18222997200@163.com>
2019-04-03 11:55:46 +0800

    DubboTransactionManagementConfiguration 提交

--- src/main/java/org/happybean/butler/dubbo/transaction/DubboTransactionAnnotationParser.java
+++ src/main/java/org/happybean/butler/dubbo/transaction/DubboTransactionAnnotationParser.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.transaction.annotation.SpringTransactionAnnotationParser;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAttribute;
 
 import java.lang.reflect.AnnotatedElement;
@@ -14,20 +15,27 @@ import java.lang.reflect.AnnotatedElement;
  * @date 2019-03-31
  * @description
  **/
-public class MySpringTransactionAnnotationParser extends SpringTransactionAnnotationParser {
+public class DubboTransactionAnnotationParser extends SpringTransactionAnnotationParser {
 
     @Override
     public TransactionAttribute parseTransactionAnnotation(AnnotatedElement ae) {
+        AnnotationAttributes attributes = null;
+        if (ae.isAnnotationPresent(Transactional.class)) {
+            attributes = AnnotatedElementUtils
+                    .getAnnotationAttributes(ae, Transactional.class.getName());
+        } else if (ae.isAnnotationPresent(DubboTransactional.class)) {
+            attributes = AnnotatedElementUtils
+                    .getAnnotationAttributes(ae, DubboTransactional.class.getName());
+        }
 
-        AnnotationAttributes attributes = AnnotatedElementUtils
-                .getAnnotationAttributes(ae, DubboTransactional.class.getName());
         if (attributes != null) {
             return parseTransactionAnnotation(attributes);
         } else {
             return null;
         }
     }
-    public TransactionAttribute parseTransactionAnnotation(AnnotationAttributes attributes) {
-        return parseTransactionAnnotation(AnnotationUtils.getAnnotationAttributes(attributes, false, false));
+
+    public TransactionAttribute parseTransactionAnnotation(DubboTransactional anno) {
+        return parseTransactionAnnotation(AnnotationUtils.getAnnotationAttributes(anno, false, false));
     }
 }
--- src/main/java/org/happybean/butler/dubbo/transaction/config/DubboTransactionManagementConfiguration.java
+++ src/main/java/org/happybean/butler/dubbo/transaction/config/DubboTransactionManagementConfiguration.java
@@ -1,9 +1,73 @@
 package org.happybean.butler.dubbo.transaction.config;
 
+import org.happybean.butler.dubbo.transaction.DubboTransactionAnnotationParser;
+import org.happybean.butler.dubbo.transaction.DubboTransactionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.Ordered;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+import org.springframework.transaction.config.TransactionManagementConfigUtils;
+import org.springframework.transaction.event.TransactionalEventListenerFactory;
+import org.springframework.transaction.interceptor.BeanFactoryTransactionAttributeSourceAdvisor;
+import org.springframework.transaction.interceptor.TransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+
 /**
  * @author wgt
  * @date 2019-03-31
  * @description
  **/
+@Configuration
 public class DubboTransactionManagementConfiguration {
+
+    protected PlatformTransactionManager txManager;
+
+    @Bean(name = TransactionManagementConfigUtils.TRANSACTION_ADVISOR_BEAN_NAME)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public BeanFactoryTransactionAttributeSourceAdvisor transactionAdvisor() {
+        BeanFactoryTransactionAttributeSourceAdvisor advisor = new BeanFactoryTransactionAttributeSourceAdvisor();
+        advisor.setTransactionAttributeSource(transactionAttributeSource());
+        advisor.setAdvice(transactionInterceptor());
+        advisor.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return advisor;
+    }
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public TransactionAttributeSource transactionAttributeSource() {
+        return new AnnotationTransactionAttributeSource(new DubboTransactionAnnotationParser());
+    }
+    @Bean(name = TransactionManagementConfigUtils.TRANSACTIONAL_EVENT_LISTENER_FACTORY_BEAN_NAME)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public TransactionalEventListenerFactory transactionalEventListenerFactory() {
+        return new TransactionalEventListenerFactory();
+    }
+    @Autowired(required = false)
+    void setConfigurers(Collection<TransactionManagementConfigurer> configurers) {
+        if (CollectionUtils.isEmpty(configurers)) {
+            return;
+        }
+        if (configurers.size() > 1) {
+            throw new IllegalStateException("Only one TransactionManagementConfigurer may exist");
+        }
+        TransactionManagementConfigurer configurer = configurers.iterator().next();
+        this.txManager = configurer.annotationDrivenTransactionManager();
+    }
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public TransactionInterceptor transactionInterceptor() {
+        TransactionInterceptor interceptor = new DubboTransactionInterceptor();
+        interceptor.setTransactionAttributeSource(transactionAttributeSource());
+        if (this.txManager != null) {
+            interceptor.setTransactionManager(this.txManager);
+        }
+        return interceptor;
+    }
 }
